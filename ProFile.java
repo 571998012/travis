45ef6e91909529d38673ed61954b88d69daf9ab3
Thanawat <thanawat.pho@crru.ac.th>
2016-11-15 14:55:05 +0700

    A

--- ProFile.java
+++ ProFile.java
@@ -1,7 +1,5 @@
 public class ProFile {
  public static void main(String args[]){
-	 System.put.prinln("FirstName:Thanawat")
-	 System.put.prinln("LastName:Paojaima")
-	  System.put.prinln("CodeName:571998012")
+	 System.put.prinln("Thanawat Paojaima")
  }
 }
